ee98bf95fa74dc4f1c0232ecc1e03d10a45d8158
Arom Ruiz <aromruiz633@gmail.com>
2025-06-30 22:54:22 -0500

    New icons

--- preguntasmodulos.gd
+++ preguntasmodulos.gd
@@ -1,14 +1,14 @@
 extends Control
 
 func _ready():
-	$GridContainer/BotonModulo1.pressed.connect(func(): _ir_a_preguntas(1))
-	$GridContainer/BotonModulo2.pressed.connect(func(): _ir_a_preguntas(2))
-	$GridContainer/BotonModulo3.pressed.connect(func(): _ir_a_preguntas(3))
-	$GridContainer/BotonModulo4.pressed.connect(func(): _ir_a_preguntas(4))
-	$GridContainer/BotonModulo5.pressed.connect(func(): _ir_a_preguntas(5))
-	$GridContainer/BotonModulo6.pressed.connect(func(): _ir_a_preguntas(6))
-	$GridContainer/BotonModulo7.pressed.connect(func(): _ir_a_preguntas(7))
-	$GridContainer/BotonModulo8.pressed.connect(func(): _ir_a_preguntas(8))
+	$ScrollContainer/TextureRect/BotonModulo1.pressed.connect(func(): _ir_a_preguntas(1))
+	$ScrollContainer/TextureRect/BotonModulo2.pressed.connect(func(): _ir_a_preguntas(2))
+	$ScrollContainer/TextureRect/BotonModulo3.pressed.connect(func(): _ir_a_preguntas(3))
+	$ScrollContainer/TextureRect/BotonModulo4.pressed.connect(func(): _ir_a_preguntas(4))
+	$ScrollContainer/TextureRect/BotonModulo5.pressed.connect(func(): _ir_a_preguntas(5))
+	$ScrollContainer/TextureRect/BotonModulo6.pressed.connect(func(): _ir_a_preguntas(6))
+	$ScrollContainer/TextureRect/BotonModulo7.pressed.connect(func(): _ir_a_preguntas(7))
+	$ScrollContainer/TextureRect/BotonModulo8.pressed.connect(func(): _ir_a_preguntas(8))
 
 func _ir_a_preguntas(modulo: int):
 	JuegoState.modulo_actual = modulo
